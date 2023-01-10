594a36bfc5b301b502bbb70c6d6ea5d599082dd8
skyv26 <dev.aakashv@gmail.com>
2023-01-10 19:34:04 +0530

    Fix the model `issue`

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
 
   has_many :expense_categories, dependent: :destroy
-  has_many :expenses, through: :expense_categories, dependent:destroy
+  has_many :expenses, through: :expense_categories, dependent: :destroy
 
   validates :name, presence: true, length: { to: 5..15 }, uniqueness: true
   validates :icon, presence: true
