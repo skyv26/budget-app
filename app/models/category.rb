2161229522cbab24da4af7ae1b73274f70609a19
skyv26 <dev.aakashv@gmail.com>
2023-01-10 18:23:54 +0530

    Generate `ExpenseCategory` model using rails

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  has_many :expense_categories, dependent: :destroy
+  has_many :expenses, through: :expense_categories, dependent:destroy
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
+
+  has_many :expense_categories, dependent: :destroy
+  has_many :categories, through: :expense_categories, dependent: :destroy
 end
