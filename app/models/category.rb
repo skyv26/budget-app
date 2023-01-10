d894f5cfa4d74e18a16b02da4f208de758c42788
skyv26 <dev.aakashv@gmail.com>
2023-01-10 20:11:19 +0530

    Fix some model bugs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   has_many :expense_categories, dependent: :destroy
   has_many :expenses, through: :expense_categories, dependent: :destroy
--- db/migrate/20230110123536_create_categories.rb
+++ db/migrate/20230110123536_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.references :author, references: :users, null: false, foreign_key: { to_table: :users, to_delete: :cascade }
+      t.references :author, references: :users, null: false, foreign_key: { to_table: :users }
       t.string :name
       t.string :icon
 
