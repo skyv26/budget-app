61683eda5ec9950f8c4748dbef5b5f4c617224df
skyv26 <dev.aakashv@gmail.com>
2023-01-10 18:28:32 +0530

    Add the `User` table validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :expenses, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 5 }
 end
