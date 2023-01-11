b18e937d1dfb38a3c0f2e8257dc5ff1afc0b4f38
skyv26 <dev.aakashv@gmail.com>
2023-01-11 21:09:03 +0530

    Generate the `Categories` controller with some file and folder structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
   get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
