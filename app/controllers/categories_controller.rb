9351ba68c983c6126c425f5bd1f61c37e4030c4b
skyv26 <dev.aakashv@gmail.com>
2023-01-13 20:03:21 +0530

    Add the `categories#new` controller actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,25 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.where(author_id: 1)
   end
 
   def show
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.author_id = 1
+    if @category.save
+      redirect_to categories_index_path
+    end
+
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
