b746bae60d4ea8dbae97256f29aac8fadf517c5b
skyv26 <dev.aakashv@gmail.com>
2023-01-15 00:49:01 +0530

    Added the style for Login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery prepend: true
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
     resources :expenses
   end
 
+  root 'categories#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "splashs#index"
 end
