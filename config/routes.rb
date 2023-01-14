2d1d555ab3644e882d18c477d1622c8c0fc54ac9
skyv26 <dev.aakashv@gmail.com>
2023-01-14 23:56:32 +0530

    generate `devise` model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   
   resources :categories do
     resources :expenses
   end
 
-  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
