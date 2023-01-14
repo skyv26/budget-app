3ab93159b4ebff05686bfa30dacd5122c76724e0
skyv26 <dev.aakashv@gmail.com>
2023-01-14 23:19:08 +0530

    Add `capybara` test for new category

--- spec/views/categories/new.html.erb_spec.rb
+++ spec/views/categories/new.html.erb_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.describe 'categories#new', type: :system do
   describe 'index page' do
     before(:each) do
-      visit new_categories_path
+      visit new_category_path
+      fill_in 'category_name', with: 'Repair'
     end
-
+    
     scenario 'can see the navigation with "Add Category" button link' do
       expect(page).to have_content("Add New Category")
     end
 
-    scenario 'can see the navigation with "Add Category" button link' do
-      expect(page).to have_content("Add New Category")
+    scenario 'can see the category name as "Repair" text' do
+      expect(page).to have_field('category_name', with: 'Repair')
     end
   end
 end
