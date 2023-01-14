0b1e0051fdf6ec544992f8dd6bdb77d21607df4d
skyv26 <dev.aakashv@gmail.com>
2023-01-14 22:49:41 +0530

    Rename the file name

--- spec/views/categories/new.html.erb_spec.rb
+++ spec/views/categories/new.html.erb_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'categories/new.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'categories#new', type: :system do
+  describe 'index page' do
+    before(:each) do
+      visit new_categories_path
+    end
+
+    scenario 'can see the navigation with "Add Category" button link' do
+      expect(page).to have_content("Add New Category")
+    end
+
+    scenario 'can see the navigation with "Add Category" button link' do
+      expect(page).to have_content("Add New Category")
+    end
+  end
 end
