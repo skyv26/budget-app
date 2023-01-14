9ebf4237a800c10f7a3816cbc0f39346f5a1358f
skyv26 <dev.aakashv@gmail.com>
2023-01-14 22:09:19 +0530

    Add `capybara-test` for splash-screen

--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'categories/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'categories/index.html.erb', type: :system do
+  describe 'index page' do
+    before(:each) do
+      @aakash = User.new(name: 'Aakash')
+      @repair = Category.new(name: 'Repair', icon: 'https://img.icons8.com/ios-filled/512/maintenance.png', author: @aakash)
+
+      visit root_path
+    end
+
+    scenario 'can see the splash screen with logo' do
+      expect(page).to have_content("Hisaab")
+    end
+
+    scenario 'can see the splash screen with "log in" button' do
+      expect(page).to have_content("Log In")
+    end
+
+    scenario 'can see the splash screen with "Sign Up" button' do
+      expect(page).to have_content("Sign Up")
+    end
+  end
 end
