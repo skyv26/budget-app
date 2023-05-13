cf1ad064863baef0352c36ed37100a38e6e01de5
Aakash Verma <dev.aakashv@gmail.com>
2023-05-13 22:24:27 +0530

    add railway host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  Rails.application.config.hosts << "budget-app-production-1d06.up.railway.app"
 end
